2bc45b49dec62784a4799b745ea11bf75eb5b171
Gabrielle Tsai <linda41436@gmail.com>
2024-09-14 12:04:08 +0800

    整合首頁 layout 與記帳流程

--- main.js
+++ main.js
@@ -1,5 +1,23 @@
 import './assets/scss/all.scss';
 import 'bootstrap/dist/js/bootstrap.min.js';
+import bootstrapMin from 'bootstrap/dist/js/bootstrap.min.js';
 import Swiper from 'swiper';
 import 'swiper/swiper-bundle.css';  // 引入 Swiper 樣式
-import 'bootstrap/dist/js/bootstrap.min.js';
+
+const numPad = new bootstrapMin.Offcanvas('#offcanvasNumPad', {
+  backdrop: 'static',
+})
+const consumptionPad = new bootstrapMin.Offcanvas('#consumptionOffcanvas')
+const categoryCheckbox = document.querySelectorAll('[for^="checkbox"]')
+const awardModal = new bootstrapMin.Modal('#awardModal')
+
+categoryCheckbox.forEach(checkbox => {
+  checkbox.addEventListener('click', () => {
+    numPad.toggle()
+  })
+})
+
+document.getElementById('numPadOk').addEventListener('click', ()=> {
+  consumptionPad.hide()
+  awardModal.show()
+})
